7d4f6ce4b9356c646ca8256abbeff3b6ebf0ed87
Bingum-de-Alwis <bingobeatz4@gmail.com>
2024-12-14 14:43:03 +0530

    Session 9

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
-
 import express from 'express';
 
 const app = express();
+import 'dotenv/config';
 import { productRouter } from './Routes/product.js';
+import { categoryRouter } from "./Routes/category.js";
+import {connectDB} from './infrastructure/db.js';
 import globalErrorHandlingMiddleware from './Routes/middlewear/global-error-handling-middlewear.js';
 
 app.use(express.json()); // For parsing JSON requests
@@ -14,6 +16,7 @@ app.use(express.json()); // For parsing JSON requests
 // });
 
 app.use('/products', productRouter)
+app.use('/categories', categoryRouter);
 app.use(globalErrorHandlingMiddleware)
-
+connectDB();
 app.listen(8000, () => console.log(`Server running on port ${8000}`));
